4f64010d8306ac59cef23d4b3576d0b3f32e2451
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2020-09-27 12:44:50 +0200

    add extended prototype

---
+++ Creational/Prototype/ExtendedPrototype/Sensor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Prototype.SimplePrototype;
+
+namespace Prototype.ExtendedPrototype
+{
+    public class Sensor : IDeepClone<Sensor>
+    {
+        private readonly int _maxValue;
+        private readonly int _minValue;
+        public int CurrentValue { get; private set; }
+        public string Name { get; }
+
+        public Sensor(int maxValue, int minValue, string name)
+        {
+            _maxValue = maxValue;
+            _minValue = minValue;
+            CurrentValue = default;
+            Name = name;
+        }
+
+        public void SetValue(int value)
+        {
+            CurrentValue = value;
+        }
+
+        public Sensor DeepClone()
+        {
+            var serializedSensor = JsonSerializer.Serialize(this);
+            return JsonSerializer.Deserialize<Sensor>(serializedSensor);
+        }
+    }
+}
